cea5365b53f7defa234f11e8aeb15b09df8ef0df
n1kk018 <nicolastorero@gmail.com>
2016-08-31 20:37:16 +0200

    projet Bank v2

--- BankWeb/src/main/java/fr/afcepf/al28/bank/web/controller/BankWebManagedBean.java
+++ BankWeb/src/main/java/fr/afcepf/al28/bank/web/controller/BankWebManagedBean.java
@@ -1,12 +1,8 @@
 package fr.afcepf.al28.bank.web.controller;
 
-import java.util.List;
-
-import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
-import javax.faces.context.FacesContext;
 
 import fr.afcepf.al28.bank.biz.IBizClient;
 import fr.afcepf.al28.bank.entity.Utilisateur;
@@ -15,8 +11,8 @@ import fr.afcepf.al28.bank.entity.Utilisateur;
 @ManagedBean(name="mbBankWeb")
 @SessionScoped
 public class BankWebManagedBean {
+	private Utilisateur user=new Utilisateur();
 	//Ici nom de l'instance (couplage fort)
-	private Utilisateur user;
 	//en meme tps pas le choix
 	@ManagedProperty(value="#{bizClient}")
 	private IBizClient bizClient;
@@ -24,9 +20,11 @@ public class BankWebManagedBean {
 	public String identification() {
 		String suite=null;
 		//simulation de recherche en BDD
-		
-		
-		
+		Object test = bizClient.Login(user);
+		if(test!=null) {
+			suite = "pages/espaceClient";
+		}
+
 		return suite;
 	}
 	
@@ -36,9 +34,7 @@ public class BankWebManagedBean {
 	public void setUser(Utilisateur user) {
 		this.user = user;
 	}
-	public IBizClient getBizClient() {
-		return bizClient;
-	}
+	
 	public void setBizClient(IBizClient bizClient) {
 		this.bizClient = bizClient;
 	}
