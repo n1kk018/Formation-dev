49ff0f852a9b5f04d70c0a41b1f011c8f602d906
Nicolas <nicolastorero@gmail.com>
2016-09-30 23:33:24 +0200

    Soap client on EJB server with wsimport by maven

--- ClientSoap/src/main/java/fr/afcepf/al28/client/ClientSoapApp.java
+++ ClientSoap/src/main/java/fr/afcepf/al28/client/ClientSoapApp.java
@@ -1,14 +1,23 @@
 package fr.afcepf.al28.client;
 
-import fr.afcepf.al28.service.TvaService;
-import fr.afcepf.al28.service.TvaServiceImplService;
+import fr.afcepf.al28.soap.devise.DeviseDTO;
+import fr.afcepf.al28.soap.devise.IServiceDevise;
+import fr.afcepf.al28.soap.devise.ServiceDeviseBeanService;
+
+/*import fr.afcepf.al28.service.TvaService;
+import fr.afcepf.al28.service.TvaServiceImplService;*/
 
 public class ClientSoapApp {
 
     public static void main(String[] args) {
-        TvaService client = (TvaService) (new TvaServiceImplService()).getTvaServiceImplPort();
+        //Soap client with wsimport batch
+        /*TvaService client = (TvaService) (new TvaServiceImplService()).getTvaServiceImplPort();
         System.out.println("Auteur: " + client.getAuteur());
-        System.out.println(client.tva(200D, 20D));
+        System.out.println(client.tva(200D, 20D));*/
+        //Soap client on EJB server with wsimport by maven
+        IServiceDevise client = (IServiceDevise) (new ServiceDeviseBeanService()).getServiceDeviseBeanPort();
+        DeviseDTO dev = client.rechercherDevise("EUR");
+        System.out.println("dev="+dev.getMonnaie()+" - "+dev.getTauxChange());
     }
 
 }
--- cliRmiEjb/src/main/java/fr/afcepf/al28/rmi/client/clientRmiApp.java
+++ cliRmiEjb/src/main/java/fr/afcepf/al28/rmi/client/clientRmiApp.java
@@ -1,15 +1,6 @@
 package fr.afcepf.al28.rmi.client;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-
-import com.organization.name.DeviseDTO;
-import com.organization.name.IServiceDevise;
-import com.organization.name.ServiceDeviseBeanService;
-
-
-
-/*import java.util.List;
+import java.util.List;
 import java.util.Properties;
 
 import javax.naming.Context;
@@ -18,50 +9,43 @@ import javax.naming.NamingException;
 
 import fr.afcepf.al28.jeeapp.ejb.api.IServiceDevise;
 import fr.afcepf.al28.jeeapp.ejb.dto.DeviseDTO;
-import fr.afcepf.al28.jeeapp.ejb.dto.PaysDTO;*/
-
+import fr.afcepf.al28.jeeapp.ejb.dto.PaysDTO;
 
 
 public class clientRmiApp {
 
-    public static void main(String[] args) {
-        //Soap Version
-        IServiceDevise client = (IServiceDevise) (new ServiceDeviseBeanService()).getServiceDeviseBeanPort();
-        DeviseDTO dev = client.rechercherDevise("EUR");
-        System.out.println("dev="+dev.getMonnaie()+" - "+dev.getTauxChange());        
-        
-        /*try {
+    public static void main(String[] args) {     
+        try {
             Properties props = new Properties();
-            props.put(Context.INITIAL_CONTEXT_FACTORY,
-                    "org.jboss.naming.remote.client.InitialContextFactory");
-                    props.put(Context.PROVIDER_URL, "remote://localhost:4447");
-                    // remote://localhost:4447 for Jboss7.1 , http-remoting://localhost:8080 for wildfly 8,9
-                    props.put(Context.SECURITY_PRINCIPAL, "nikko"); // username : "admin" , "guest" , "..."
-                    props.put(Context.SECURITY_CREDENTIALS, "%Nikko12"); //password : "pwd", "guest007"
-                    //avec utilisateur ajouté via la commande JBOSS_7_HOME/bin/add-user
-                    //et roles associés admin,guest,.... sur partie "ApplicationRealm" .
-                    props.put("jboss.naming.client.ejb.context", true); //indispensable pour accès @Remote
-                    Context jndiContext = new InitialContext(props);
-                    String jndiName = "jeeapp-ear/jeeapp-ejb-impl/ServiceDeviseBean!fr.afcepf.al28.jeeapp.ejb.api.IServiceDevise";
-                    // sans "ejb:" et sans // pour version de jndiName sans jboss-ejb-client.properties et sans
-                    // props.setProperty(Context.URL_PKG_PREFIXES,"org.jboss.ejb.client.naming");
-                    IServiceDevise ejbDevise = (IServiceDevise) jndiContext.lookup(jndiName);
-                    System.out.println("30 euros= " + ejbDevise.convertir(30.0,"EUR","USD") + " dollars");
-                    System.out.println(ejbDevise.rechercherDevise("JPY"));
-                    List<DeviseDTO> list = ejbDevise.toutesDevises();
-                    System.out.println("Toutes les devises : ");
-                    for (DeviseDTO deviseDTO : list) {
-                        System.out.println(deviseDTO);
-                    }
-                    List<PaysDTO> list2 = ejbDevise.listePaysPartageantDevise("EUR");
-                    System.out.println("Tous les pays en euros: ");
-                    for (PaysDTO paysDTO : list2) {
-                        System.out.println(paysDTO);
-                    }    
+            props.put(Context.INITIAL_CONTEXT_FACTORY,"org.jboss.naming.remote.client.InitialContextFactory");
+            props.put(Context.PROVIDER_URL, "remote://localhost:4447");
+            // remote://localhost:4447 for Jboss7.1 , http-remoting://localhost:8080 for wildfly 8,9
+            props.put(Context.SECURITY_PRINCIPAL, "nikko"); // username : "admin" , "guest" , "..."
+            props.put(Context.SECURITY_CREDENTIALS, "%Nikko12"); //password : "pwd", "guest007"
+            //avec utilisateur ajouté via la commande JBOSS_7_HOME/bin/add-user
+            //et roles associés admin,guest,.... sur partie "ApplicationRealm" .
+            props.put("jboss.naming.client.ejb.context", true); //indispensable pour accès @Remote
+            Context jndiContext = new InitialContext(props);
+            String jndiName = "jeeapp-ear/jeeapp-ejb-impl/ServiceDeviseBean!fr.afcepf.al28.jeeapp.ejb.api.IServiceDevise";
+            // sans "ejb:" et sans // pour version de jndiName sans jboss-ejb-client.properties et sans
+            // props.setProperty(Context.URL_PKG_PREFIXES,"org.jboss.ejb.client.naming");
+            IServiceDevise ejbDevise = (IServiceDevise) jndiContext.lookup(jndiName);
+            System.out.println("30 euros= " + ejbDevise.convertir(30.0,"EUR","USD") + " dollars");
+            System.out.println(ejbDevise.rechercherDevise("JPY"));
+            List<DeviseDTO> list = ejbDevise.toutesDevises();
+            System.out.println("Toutes les devises : ");
+            for (DeviseDTO deviseDTO : list) {
+                System.out.println(deviseDTO);
+            }
+            List<PaysDTO> list2 = ejbDevise.listePaysPartageantDevise("EUR");
+            System.out.println("Tous les pays en euros: ");
+            for (PaysDTO paysDTO : list2) {
+                System.out.println(paysDTO);
+            }    
         }
-            catch (NamingException e){
-                e.printStackTrace();
-            }*/
+        catch (NamingException e){
+            e.printStackTrace();
+        }
     }
 
 }
