a4714b44954418aee50fb8b92b410f045a45d801
n1kk018 <nicolastorero@gmail.com>
2016-10-06 18:35:54 +0200

    BPEL

--- springCxfWeb/src/main/java/fr/afcepf/al28/service/ICalculateur.java
+++ springCxfWeb/src/main/java/fr/afcepf/al28/service/ICalculateur.java
@@ -1,7 +1,11 @@
 package fr.afcepf.al28.service;
 
 import javax.jws.WebParam;
+import javax.jws.WebService;
 
+import org.springframework.stereotype.Service;
+
+@WebService
 public interface ICalculateur {
     public double addition(@WebParam(name="v1")double v1, @WebParam(name="v2")double v2);
     public double multiplication(@WebParam(name="v1")double v1, @WebParam(name="v2")double v2);
