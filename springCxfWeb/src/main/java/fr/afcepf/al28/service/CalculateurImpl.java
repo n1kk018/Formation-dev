3864205748dd03b01253c88101f57a886fea5156
n1kk018 <nicolastorero@gmail.com>
2016-10-07 11:21:48 +0200

    BPEL

--- springCxfWeb/src/main/java/fr/afcepf/al28/service/CalculateurImpl.java
+++ springCxfWeb/src/main/java/fr/afcepf/al28/service/CalculateurImpl.java
@@ -10,11 +10,13 @@ public class CalculateurImpl implements ICalculateur {
 
     @Override
     public double addition(double paramV1, double paramV2) {
+        System.out.println("========================>addition");
         return paramV1+paramV2;
     }
 
     @Override
     public double multiplication(double paramV1, double paramV2) {
+        System.out.println("========================>multiplication");
         return paramV1*paramV2;
     }
 
