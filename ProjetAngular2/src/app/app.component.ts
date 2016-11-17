527769bffb226edc5a2fd5579ddebadb81e49e71
n1kk018 <nicolastorero@gmail.com>
2016-11-17 17:17:28 +0100

    projet angular 2

--- ProjetAngular2/src/app/app.component.ts
+++ ProjetAngular2/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @Input()
   title = 'app works!';
+
+  public alert() {
+    alert('bloup');
+  }
 }
